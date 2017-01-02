850688f92590eb77e093d988578dd71c5f7f7cd2
Christophe <christophe.thomas.29@outlook.fr>
2017-01-02 23:05:48 +0100

    Formatting: JS code for data tables.

--- CapaNavDoc/Scripts/MyDataTable.js
+++ CapaNavDoc/Scripts/MyDataTable.js
@@ -1,42 +1,65 @@
-﻿function SetDataTable(
-    dataTableId, ajaxSource, columnsDefArray, 
-    updatectionUrl, updateFormTitle, updateFormWidth, 
-    deleteActionUrl, deleteFormTitle, deleteFormWidth) {
+﻿function GetShowDialog(title, width, url, editFormId, dataTableId) {
+    var showDialog = "ShowDialog(\"";
+    showDialog += title;
+    showDialog += "\", \"";
+    showDialog += width;
+    showDialog += "\", \"";
+    showDialog += url;
+    showDialog += "\", \"";
+    showDialog += editFormId,
+    showDialog += "\", \"";
+    showDialog += dataTableId;
+    showDialog += "\")";
+    return showDialog;
+}
+
+function GetHtmlImage(icon, alt, dialogTitle, dialogWidth, url, editFormId, dataTableId) {
+    var result = "<img src=";
+    result += "\'" + icon + "\' ";
+    result += "alt=\'" + alt + "\' ";
+    result += "onclick=\'" + GetShowDialog(dialogTitle, dialogWidth, url, editFormId, dataTableId) + "\' />";  
+    return result;
+}
+
+function  GetHtmlLink(cssClass, text, dialogTitle, dialogWidth, url, editFormId, dataTableId) {
+    var result = "<a class=\'";
+    result += cssClass;
+    result += "\' onclick=\'" + GetShowDialog(dialogTitle, dialogWidth, url, editFormId, dataTableId) + "\'>";
+    result += text;
+    result += "</a>";
+    return result;
+}
+
+
+function SetDataTable(dtId, ajaxSrc, colDef, uUrl, uFrmTitle, uFrmW, dUrl, dFrmTitle, dFrmW) {
+
+    var editForm = "EditForm";
+    var confirmForm = "ConfirmationForm";
 
     var idColumn = [{"sName": "ID", "visible": false}];
     var specialColumns = [
-        {
-            "sName": "Update",
+        {   "sName": "Update",
             "mRender": function(data, type, full) {
-                var url = updatectionUrl + "?id=" + full[0];
-                var result = '<img src=\"/Content/Icons/Pencil-icon.png\" alt=\"Crayon\" onclick=\'ShowDialog(\"' + updateFormTitle + '\", ' + updateFormWidth + ', \"' + url + '\", \"EditForm\", \"' + dataTableId + '\")\' />';
-                return result;
-            }
-        },
-        {
-            "sName": "Delete",
+                var url = uUrl + "?id=" + full[0];
+                return GetHtmlImage("/Content/Icons/Pencil-icon.png", "Pencil", uFrmTitle, uFrmW, url, editForm, dtId);
+            }},
+        {   "sName": "Delete",
             "mRender": function(data, type, full) {
-                var url = deleteActionUrl + "?id=" + full[0];
-                var result = '<img src=\"/Content/Icons/Close-2-icon.png\" alt=\"Crayon\" onclick=\'ShowDialog(\"' + deleteFormTitle + '\", ' + deleteFormWidth + ', \"' + url + '\", \"ConfirmationForm\", \"' + dataTableId + '\")\' />';
-                return result;
-            }
-        }
-    ];
-    var all = idColumn.concat(columnsDefArray).concat(specialColumns);
+                var url = dUrl + "?id=" + full[0];
+                return GetHtmlImage("/Content/Icons/Close-2-icon.png", "Cross", dFrmTitle, dFrmW, url, confirmForm, dtId);
+            }}];
+    var all = idColumn.concat(colDef).concat(specialColumns);
 
-    var table = $("#" + dataTableId)
-        .dataTable({
+    $("#" + dtId).dataTable({
             "bServerSide": true,
-            "sAjaxSource": ajaxSource,
+            "sAjaxSource": ajaxSrc,
             "bProcessing": true,
             "aoColumns": all,
             //"fnServerParams": function (aoData) {
             //    aoData.push({ "name": "mycolumn1", "value": $('#col1').val() });
             //    aoData.push({ "name": "mycolumn2", "value": $('#col2').val() });
             //},
-            "columnDefs": [{ "className": "CenteredColumn", "targets": [all.length-2, all.length-1] }]
-        });
-
+            "columnDefs": [{ "className": "CenteredColumn", "targets": [all.length-2, all.length-1] }]});
     //$("#col1, #col2")
     //    .bind("keyup",
     //        function () {
@@ -44,55 +67,43 @@
     //        });
 };
 
-function SetMasterDataTable(
-    dataTableId, ajaxSource, columnsDefArray, 
-    updateActionUrl, updateFormTitle, updateFormWidth, 
-    deleteActionUrl, deleteFormTitle, deleteFormWidth, 
-    detailViewUrl) {
+function SetMasterDataTable(dtId, ajaxSrc, colDef, uUrl, uFrmTitle, uFrmW, dUrl, dFrmTitle, dFrmW, dvUrl) {
+
+    var editForm = "EditForm";
+    var confirmForm = "ConfirmationForm";
 
     var idColumn = [
-        {
-            "sName": "ID",
+        {   "sName": "ID",
             "visible": false
         },
-        {
-            "bSortable": false,
+        {   "bSortable": false,
             "bSearchable": false,
             "mRender": function(data, type, full) {
-                return '<img src="/Content/Icons/Add-Green-Button-icon.png" alt="expand/collapse" rel="' + full[0] + '"/>';
-            }
-        }
-    ];
+            return '<img src="/Content/Icons/Add-Green-Button-icon.png" alt="Expand/Collapse" rel="' + full[0] + '"/>';
+            }}];
     var specialColumns = [
-        {
-            "sName": "Update",
+        {   "sName": "Update",
             "mRender": function (data, type, full) {
-                var url = updateActionUrl + "?id=" + full[0];
-                var result = '<img src=\"/Content/Icons/Pencil-icon.png\" alt=\"Crayon\" onclick=\'ShowDialog(\"' + updateFormTitle + '\", ' + updateFormWidth + ', \"' + url + '\", \"EditForm\", \"' + dataTableId + '\")\' />';
-                return result;
-            }
-        },
-        {
-            "sName": "Delete",
+                var url = uUrl + "?id=" + full[0];
+                return GetHtmlImage("/Content/Icons/Pencil-icon.png", "Pencil", uFrmTitle, uFrmW, url, editForm, dtId); 
+            }},
+        {   "sName": "Delete",
             "mRender": function (data, type, full) {
-                var url = deleteActionUrl + "?id=" + full[0];
-                var result = '<img src=\"/Content/Icons/Close-2-icon.png\" alt=\"Crayon\" onclick=\'ShowDialog(\"' + deleteFormTitle + '\", ' + deleteFormWidth + ', \"' + url + '\", \"ConfirmationForm\", \"' + dataTableId + '\")\' />';
-                return result;
-            }
-        }
-    ];
-    var all = idColumn.concat(columnsDefArray).concat(specialColumns);
+                var url = dUrl + "?id=" + full[0];
+                return GetHtmlImage("/Content/Icons/Close-2-icon.png", "Cross", dFrmTitle, dFrmW, url, confirmForm, dtId);
+            }}];
+    var all = idColumn.concat(colDef).concat(specialColumns);
 
-    var table = $("#" + dataTableId)
+    var table = $("#" + dtId)
         .dataTable({
             "bServerSide": true,
-            "sAjaxSource": ajaxSource,
+            "sAjaxSource": ajaxSrc,
             "bProcessing": true,
             "aoColumns": all,
             "columnDefs": [{ "className": "CenteredColumn", "targets": [all.length - 2, all.length - 1] }]
         });
 
-    $("#" + dataTableId + " tbody").on("click", "tr td img", function () {
+    $("#" + dtId + " tbody").on("click", "tr td img", function () {
         var nTr = this.parentNode.parentNode;
         var image = this;
         if (this.src.match("Minus-icon")) {
@@ -102,8 +113,8 @@ function SetMasterDataTable(
         else {
             this.src = "/Content/Icons/Minus-icon.png";
             var id = $(this).attr("rel");
-            $.get(detailViewUrl, { id: id }, function (details) { table.fnOpen(nTr, details, "DetailedRow") });
-            $("#" + dataTableId + " tbody").on("click", "tr td input[type=button]", function() {
+            $.get(dvUrl, { id: id }, function (details) { table.fnOpen(nTr, details, "DetailedRow") });
+            $("#" + dtId + " tbody").on("click", "tr td input[type=button]", function() {
                 var form = $("#DetailForm");
                 $.ajax(
                 {
@@ -122,56 +133,40 @@ function SetMasterDataTable(
     });
 };
 
-function SetEquipmentDataTable(
-    dataTableId, ajaxSource, columnsDefArray, 
-    updatectionUrl, updateFormTitle, updateFormWidth, 
-    deleteActionUrl, deleteFormTitle, deleteFormWidth,
-    updateMonitoringActionUrl, updateMonitoringFormTitle, updateMonitoringFormWidth,
-    updateCentersActionsUrl, updateCentersActionsFormTitle, updateCentersActionsFormWidth) {
+function SetEquipmentDataTable(dtId, ajaxSrc, colDef, uUrl, uFrmTitle, uFrmW, dUrl, dFrmTitle, dFrmW, uUrl2, uFrmTitle2, uFrmW2, uUrl3, uFrmTitle3, uFrmW3) {
+
+    var editForm = "EditForm";
+    var confirmForm = "ConfirmationForm";
+    var monitoringCssClass = "CursorPointer";
 
     var idColumn = [{"sName": "ID", "visible": false}];
     var specialColumns = [
-        {
-            "sName": "CentersActions",
+        {   "sName": "CentersActions",
             "mRender": function(data, type, full) {
-                var url = updateCentersActionsUrl + "?id=" + full[0];
-                var result = '<img src=\"/Content/Icons/Zoom-icon.png\" alt=\"Magnifier\" onclick=\'ShowDialog(\"' + updateCentersActionsFormTitle + '\", ' + updateCentersActionsFormWidth + ', \"' + url + '\", \"EditForm\", \"' + dataTableId + '\")\' />';
-                return result;
-            }
-        },
-        {
-            "sName": "Monitoring",
+                var url = uUrl3 + "?id=" + full[0];
+                return GetHtmlImage("/Content/Icons/Zoom-icon.png", "Magnifier", uFrmTitle3, uFrmW3, url, editForm, dtId);
+            }},
+        {   "sName": "Monitoring",
             "mRender": function(data, type, full) {
-                var url = updateMonitoringActionUrl + "?id=" + full[0];
-                var result = '<a class="CursorPointer" onclick=\'ShowDialog(\"' + updateMonitoringFormTitle + '\", ' + updateMonitoringFormWidth + ', \"' + url + '\", \"EditForm\", \"' + dataTableId + '\")\'>' + full[10] + "</a>";
-                return result;
-            }
-        },
-        {
-            "sName": "Update",
+                var url = uUrl2 + "?id=" + full[0];
+                return GetHtmlLink(monitoringCssClass, full[10], uFrmTitle2, uFrmW2, url, editForm, dtId);
+            }},
+        {   "sName": "Update",
             "mRender": function(data, type, full) {
-                var url = updatectionUrl + "?id=" + full[0];
-                var result = '<img src=\"/Content/Icons/Pencil-icon.png\" alt=\"Crayon\" onclick=\'ShowDialog(\"' + updateFormTitle + '\", ' + updateFormWidth + ', \"' + url + '\", \"EditForm\", \"' + dataTableId + '\")\' />';
-                return result;
-            }
-        },
-        {
-            "sName": "Delete",
+                var url = uUrl + "?id=" + full[0];
+                return GetHtmlImage("/Content/Icons/Pencil-icon.png", "Pencil", uFrmTitle, uFrmW, url, editForm, dtId);
+            }},
+        {   "sName": "Delete",
             "mRender": function(data, type, full) {
-                var url = deleteActionUrl + "?id=" + full[0];
-                var result = '<img src=\"/Content/Icons/Close-2-icon.png\" alt=\"Crayon\" onclick=\'ShowDialog(\"' + deleteFormTitle + '\", ' + deleteFormWidth + ', \"' + url + '\", \"ConfirmationForm\", \"' + dataTableId + '\")\' />';
-                return result;
-            }
-        }
-    ];
-    var all = idColumn.concat(columnsDefArray).concat(specialColumns);
+                var url = dUrl + "?id=" + full[0];
+                return GetHtmlImage("/Content/Icons/Close-2-icon.png", "Cross", dFrmTitle, dFrmW, url, confirmForm, dtId);
+            }}];
+    var all = idColumn.concat(colDef).concat(specialColumns);
 
-    $("#" + dataTableId)
-        .dataTable({
+    $("#" + dtId).dataTable({
             "bServerSide": true,
-            "sAjaxSource": ajaxSource,
+            "sAjaxSource": ajaxSrc,
             "bProcessing": true,
             "aoColumns": all,
-            "columnDefs": [{ "className": "CenteredColumn", "targets": [all.length-4, all.length-2, all.length-1] }]
-        });
+            "columnDefs": [{ "className": "CenteredColumn", "targets": [all.length-4, all.length-2, all.length-1] }]});
 };
